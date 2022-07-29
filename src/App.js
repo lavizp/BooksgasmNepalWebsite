924df45eb6f0c0339e941cf256e6db6ffc891ab2
Laviz-pand <pandeylaviz@gmail.com>
2022-07-29 16:15:58 +0545

    Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import NavBar from "./Components/Navbar/Navbar/NavBar";
+import NavBar from "./Components/Navbar/NavBar";
+import BannerMain from "./Components/Banners/BannerMain";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
       <NavBar />
+      <BannerMain />
     </>
   );
 }
---
+++ src/Components/Banners/BannerMain.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./banner.css";
+import BookImage from "./Book.png";
+import BookListImage from "./BookPile.jpg";
+
+export default function BannerMain() {
+  return (
+    <div className="banner-container">
+      <img src={BookImage} className="background"></img>
+      <img src={BookListImage} className="circle-image" />
+      <div className="text">
+        <h1>
+          Buy Books at
+          <br />
+          an Affordeble
+          <br />
+          Rrice
+        </h1>
+      </div>
+    </div>
+  );
+}
