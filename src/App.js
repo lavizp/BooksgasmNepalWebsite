c61e8dba0db46308c3b9d89de5ce4412e8b761d6
Laviz-pand <pandeylaviz@gmail.com>
2022-07-29 13:47:39 +0545

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import NavBar from "./Components/Navbar/Navbar/NavBar";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
--- src/Components/Navbar/Navbar/NavBar.js
+++ src/Components/Navbar/Navbar/NavBar.js
@@ -1,41 +1,46 @@
-import React from "react";
-import "./navbar.css";
+import React, { useState } from "react";
 import logo from "./logo.png";
+import "./navbar.css";
 import { FaShoppingCart } from "react-icons/fa";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function NavBar() {
   return (
-    <div className="navbar-container">
-      <a href="" className="Logo">
-        <img src={logo} />
-        <h1>BooksGasm</h1>
-      </a>
-      <nav className="menuitems">
-        <ul>
-          <li>
-            <a href="">Catogeries</a>
-            <ul className="dropdown-list">
-              <li>hello sir</li>
-              <li>hello sir</li>
-            </ul>
-          </li>
-          <li>
-            <a href="">Combo-Deals</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contacts</a>
-          </li>
-
-          <li>
-            <a href="">
-              <FaShoppingCart color="grey" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home" className="Logo">
+            <img src={logo} />
+            <h1>BooksGasm</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <NavDropdown title="Catogeries" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Self-Help
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Fiction</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Money</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  All Catogeries
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#features">Combo-Deals</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Contacts</Nav.Link>
+              <Nav.Link href="#pricing">
+                <FaShoppingCart />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
