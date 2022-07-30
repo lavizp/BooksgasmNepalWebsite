1a2a56f6fb0f169dcdfc5ed14fba474609e5c335
Laviz-pand <pandeylaviz@gmail.com>
2022-07-30 08:41:40 +0545

    Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import NavBar from "./Components/Navbar/NavBar";
 import BannerMain from "./Components/Banners/BannerMain";
+
+import { Routes, Route } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./Pages/HomePage";
 function App() {
   return (
     <>
-      <NavBar />
-      <BannerMain />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="" element />
+      </Routes>
     </>
   );
 }
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Components/Navbar/NavBar";
+import BannerMain from "../Components/Banners/BannerMain";
+
+export default function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <BannerMain />
+    </>
+  );
+}
