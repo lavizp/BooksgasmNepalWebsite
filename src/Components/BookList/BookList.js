53c0522a8020eb3e49c964d03feaf50450bf4996
Laviz-pand <pandeylaviz@gmail.com>
2022-08-05 10:27:06 +0545

    Update on Catogery Page

--- src/Components/BookList/BookList.js
+++ src/Components/BookList/BookList.js
@@ -5,7 +5,7 @@ import "./booklist.css";
 
 export default function BookList({ title }) {
   const [bookListData, setBookListData] = useState([]);
-
+  const [bestSellers, setBestSellers] = useState([]);
   useEffect(() => {
     db.collection("bookList").onSnapshot((snapshot) => {
       setBookListData([]);
@@ -16,22 +16,27 @@ export default function BookList({ title }) {
       });
     });
   }, []);
+
   console.log(bookListData);
+  console.log(bestSellers);
+
   return (
     <>
       <div className="list-title">
         <h1 className="black">{title}</h1> <h1 className="yellow">Books:</h1>
       </div>
       <div className="book-list">
-        {bookListData.map((item, index) => (
-          <SingleBookElement
-            key={index}
-            title={item.title}
-            author={item.author}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
+        {bookListData
+          .filter((itm) => itm.price <= 1400)
+          .map((item, index) => (
+            <SingleBookElement
+              key={index}
+              title={item.title}
+              author={item.author}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
       </div>
     </>
   );
--- src/Pages/Catogeries/SingleCatogeryPage.js
+++ src/Pages/Catogeries/SingleCatogeryPage.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import db from "../../Data/firebase";
+import BookList from "../../Components/BookList/BookList";
+import SingleBookElement from "../../Components/SingleBook/SingleBookElement";
 
 export default function SingleCatogeryPage() {
+  const [bookListData, setBookListData] = useState([]);
+  const [bestSellers, setBestSellers] = useState([]);
+  useEffect(() => {
+    db.collection("bookList").onSnapshot((snapshot) => {
+      setBookListData([]);
+      snapshot.forEach((element) => {
+        var data = element.data();
+
+        setBookListData((arr) => [...arr, data]);
+      });
+    });
+  }, []);
   const { catogery } = useParams();
   return (
     <div>
       <h1>{catogery}</h1>
+      {bookListData
+        .filter((itm) => itm.catogery == catogery)
+        .map((item, index) => (
+          <SingleBookElement
+            key={index}
+            title={item.title}
+            author={item.author}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
     </div>
   );
 }
