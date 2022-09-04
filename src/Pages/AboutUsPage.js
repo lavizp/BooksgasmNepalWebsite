b9f376a3c566045d7eb4c788e08e7bc26475107d
Laviz-pand <pandeylaviz@gmail.com>
2022-09-04 15:35:43 +0545

    About Page Details

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -4,7 +4,32 @@ export default function AboutUsPage() {
   return (
     <>
       <NavBar />
-      <h1>Hami Khattra ho</h1>
+      <h1>About Us</h1>
+      <h1>
+        BooksGasm was simply born out of the careful observation in the simple
+        yet urgent problem of any scholar. We noticed that hundreds of
+        used-books had to be dumped in the bookshelves after being read. That
+        book could have been of a great significance to any other reader. But,
+        due to the lack of connectivity and lack of a trusted party, the used
+        book couldn’t find that other reader. Simply wasting a book with great
+        academic and scholarly value was heartbreaking for the buyer and for we
+        observers as well. We sought the solution. The seeking gave birth to the
+        trusted third party seller, “ BooksGasm”. After months of research on
+        the books of such a category and prospect, we launched our website,
+        “Booksgasmnepal.com”. The founding members: Rahul Yadav, Reyan Kumar
+        Sapkota, Prashant Yadav and Laviz Pandey, aim to encapsulate the agenda
+        of Shared-Reading within the entire country. Business and monetary value
+        is a secondary aspect of our initiative. Our primary goal is to match
+        the used-book to the prospective buyer within a click of his finger, so
+        that the book and the buyer’s longing to learn does not go in vain. The
+        initiative also serves the Environment. Buying a used book at an
+        arguable reasonable price serves the 3R policy (Reduce, Reuse, Recycle).
+        Nevertheless, our goal is to ensure profit for both seller and buyer,
+        and ensure that the value of a significantly relevant book does not die
+        in a bookshelf. Furthermore, we try to become the best book providing
+        online platform in the country for students of all levels here. We are
+        here to make your learning journey much more easier here.
+      </h1>
     </>
   );
 }
