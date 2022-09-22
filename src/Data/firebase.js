66ffa8076ac5ab5fc9e0e14f5269ba7c8ec03753
Laviz-pand <pandeylaviz@gmail.com>
2022-09-22 10:11:27 +0545

    Auth Init

--- src/Data/firebase.js
+++ src/Data/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import "firebase/auth";
 
 const firebaseConfig = firebase.initializeApp({
   apiKey: "AIzaSyChvGETPVdCzS-6FdBRAcS0DH5MP5htwSA",
@@ -10,5 +11,6 @@ const firebaseConfig = firebase.initializeApp({
   appId: "1:866012951786:web:bc32810dca8c686c25d393",
 });
 
+export const auth = firebaseConfig.auth();
 const db = firebaseConfig.firestore();
 export default db;
