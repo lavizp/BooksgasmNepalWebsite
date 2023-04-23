c7c1cdfdfcb230c4354fd16180c57d21b83ab4d9
Laviz-pand <pandeylaviz@gmail.com>
2023-04-23 21:34:41 +0545

    [Update] Homepage UI and Footer Section

---
+++ client/components/productCard.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+const ProductCard = () => {
+    return (
+        <Link
+            href={`/product/`}
+            className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
+        >
+            <Image
+                width={500}
+                height={500}
+                src="/p7.png"
+                alt="asd"
+            />
+            <div className="p-4 text-black/[0.9]">
+                <h2 className="text-lg font-medium">Sahi Product</h2>
+                <div className="flex items-center text-black/[0.5]">
+                    <p className="mr-2 text-lg font-semibold">
+                        &#8377;12
+                    </p>
+
+                    {true && (
+                        <>
+                            <p className="text-base  font-medium line-through">
+                                &#8377;12
+                            </p>
+                            <p className="ml-auto text-base font-medium text-green-500">
+                                10
+                                % off
+                            </p>
+                        </>
+                    )}
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default ProductCard;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Header } from '@/components/header'
+import Footer from '@/components/footer'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
@@ -9,5 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
     </Head>
     <Header/>
     <Component {...pageProps} />
+    <Footer/>
   </>
 }
