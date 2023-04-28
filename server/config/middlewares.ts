75290be7629090f30efbf21297dda19c7b0f7d1a
Laviz-pand <pandeylaviz@gmail.com>
2023-04-28 17:17:28 +0545

    [Init] Strapi Init

---
+++ server/config/middlewares.ts
@@ -0,0 +1,24 @@
+export default [
+  "strapi::errors",
+  {
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "upgrade-insecure-requests": "",
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
